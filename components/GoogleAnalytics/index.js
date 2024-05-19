ad61d2162a76d1f85dbebd6b455d9c8b96c2e70c
PC53 <s2104454@ed.ac.uk>
2024-05-19 23:26:34 +0530

    google analytics

--- components/GoogleAnalytics/index.js
+++ components/GoogleAnalytics/index.js
@@ -1,9 +1,29 @@
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
 
 
 const GoogleAnalytics = () => {
   const GA_TRACKING_ID = 'G-YHK5375TFR';
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      console.log('Tracking page view for:', url);
+      window.gtag('config', GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+    // Track initial page load
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: window.location.pathname,
+    });
+
+    // Track page changes
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
